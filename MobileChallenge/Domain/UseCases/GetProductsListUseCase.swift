c93e6720974f94494f783ed893dba0673f9f2505
thomas.pereira.ptc <thomas.pereira@jumia.com>
2023-01-30 18:33:33 -0300

    Created use case for products list

---
+++ MobileChallenge/Domain/UseCases/GetProductsListUseCase.swift
@@ -0,0 +1,26 @@
+//
+//  GetProductsListUseCase.swift
+//  MobileChallenge
+//
+//  Created by thomas pereira on 27/01/2023.
+//
+
+import Foundation
+import Combine
+
+protocol GetProductsListUseCase {
+    func getProductsList() -> AnyPublisher<ProductsList, Error>
+}
+
+final class DefaultGetProductsListUseCase: GetProductsListUseCase {
+    
+    private let productsListRepository: ProductsListRepository
+    
+    init(productsListRepository: ProductsListRepository) {
+        self.productsListRepository = productsListRepository
+    }
+    
+    func getProductsList() -> AnyPublisher<ProductsList, Error> {
+        return productsListRepository.fetchProductsList()
+    }
+}
