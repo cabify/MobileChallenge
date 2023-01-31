1440b6c54aff24f81af605bbde2a841f6d74fcf5
thomas.pereira.ptc <thomas.pereira@jumia.com>
2023-01-31 07:14:10 -0300

    Removed identifiable protocol from the domain model

--- MobileChallenge/Domain/Entities/ProductsList.swift
+++ MobileChallenge/Domain/Entities/ProductsList.swift
@@ -9,10 +9,7 @@ import Foundation
 
 struct ProductsList: Equatable {
     
-    struct Product: Equatable, Identifiable {
-        typealias Identifier = UUID
-        
-        let id: Identifier = Identifier()
+    struct Product: Equatable {
         let code: String
         let name: String
         let price: Double
