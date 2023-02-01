d34df877531f23a9443e4ef288ca4d60a4da8b4a
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-01 15:07:52 -0300

    Implemented custom view to add or remove product from cart

---
+++ MobileChallenge/Presentation/ProductsList/UI/CartQuantityView.swift
@@ -0,0 +1,50 @@
+//
+//  CartQuantityView.swift
+//  MobileChallenge
+//
+//  Created by thomas pereira on 01/02/2023.
+//
+
+import SwiftUI
+
+struct CartQuantityView: View {
+    
+    @State var cartQuantity: Int
+    
+    var body: some View {
+        HStack {
+            // Minus button
+            quantityButton(imageNamed: "minus.rectangle") {
+                cartQuantity -= 1
+            }.disabled(cartQuantity <= 0)
+            
+            // Current quantity
+            Text("\(cartQuantity)")
+                .font(.system(size: 16))
+            
+            // Plus button
+            quantityButton(imageNamed: "plus.rectangle") {
+                cartQuantity += 1
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func quantityButton(imageNamed: String, onTap: @escaping () -> Void) -> some View {
+        Button {
+            onTap()
+        } label: {
+            Image(systemName: imageNamed)
+        }
+        .tint(.black)
+    }
+}
+
+// MARK: - Preview
+#if DEBUG
+struct CartQuantityView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartQuantityView(cartQuantity: 0)
+    }
+}
+#endif
