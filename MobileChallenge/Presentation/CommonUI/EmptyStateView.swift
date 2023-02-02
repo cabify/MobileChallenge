5b9ab303baec4acc1eb793a6348ef9a0561dbf71
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-02 11:39:47 -0300

    Created custom view to show empty states

---
+++ MobileChallenge/Presentation/CommonUI/EmptyStateView.swift
@@ -0,0 +1,59 @@
+//
+//  EmptyStateView.swift
+//  MobileChallenge
+//
+//  Created by thomas pereira on 02/02/2023.
+//
+
+import SwiftUI
+
+struct EmptyStateView: View {
+    
+    enum EmptyType {
+        case products
+        case cart
+        
+        var imageNamed: String {
+            switch self {
+            case .products: return "tray.fill"
+            case .cart: return "cart.fill"
+            }
+        }
+        
+        var emptyText: String {
+            switch self {
+            case .products: return "Ops... we are updating our stocks!"
+            case .cart: return "Your cart is empty. Go enjoy our discounts!"
+            }
+        }
+    }
+    
+    var emptyType: EmptyType
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Image(systemName: emptyType.imageNamed)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 120, height: 120)
+                .foregroundColor(.purple)
+            
+            Text(emptyType.emptyText)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.purple)
+        }
+        .padding(.all, 40)
+    }
+}
+
+// MARK: - Preview
+#if DEBUG
+struct EmptyStateView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            EmptyStateView(emptyType: .products)
+            EmptyStateView(emptyType: .cart)
+        }
+    }
+}
+#endif
