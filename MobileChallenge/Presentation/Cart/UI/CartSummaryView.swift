fe516446d8fb19d77c05c722b59d7398d1b7013b
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-08 10:36:16 -0300

    Implemented reusable cart summary view

--- MobileChallenge/Presentation/Cart/UI/CartSummaryView.swift
+++ MobileChallenge/Presentation/Cart/UI/CartSummaryView.swift
@@ -5,4 +5,73 @@
 //  Created by thomas pereira on 08/02/2023.
 //
 
-import Foundation
+import SwiftUI
+
+struct CartSummaryView: View {
+    
+    var cartViewModel: CartViewModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            // Subtotal
+            summaryItem(
+                titleText: "Subtotal",
+                valueText: cartViewModel.formattedSubtotal
+            )
+            
+            // Discounts
+            if cartViewModel.showDiscounts {
+                VStack(alignment: .leading, spacing: 5) {
+                    ForEach(cartViewModel.discounts) { aDiscount in
+                        summaryItem(
+                            titleText: aDiscount.text,
+                            valueText: aDiscount.formattedValue,
+                            isDiscount: true
+                        )
+                    }
+                }
+            }
+            
+            Divider()
+                .frame(width: .infinity)
+            
+            // Total
+            summaryItem(
+                titleText: "Total",
+                valueText: cartViewModel.formattedTotal,
+                isHighlighted: true
+            )
+        }
+        .padding(EdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20))
+    }
+    
+    @ViewBuilder
+    private func summaryItem(titleText: String, valueText: String, isHighlighted: Bool = false, isDiscount: Bool = false) -> some View {
+        let fontSize: CGFloat = isDiscount ? 16 : isHighlighted ? 22 : 18
+        let fontWeight: Font.Weight = isDiscount ? .regular : .medium
+        let textColor: Color = isDiscount ? .purple : .black
+        
+        HStack(alignment: .center, spacing: 10) {
+            Text(titleText)
+                .font(.system(size: fontSize, weight: fontWeight))
+                .foregroundColor(textColor)
+                .multilineTextAlignment(.leading)
+            
+            Spacer()
+            
+            Text(valueText)
+                .font(.system(size: fontSize, weight: fontWeight))
+                .foregroundColor(textColor)
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
+
+// MARK: - Preview
+#if DEBUG
+struct CartSummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        CartSummaryView(cartViewModel: CartViewModel.preview)
+    }
+}
+#endif
