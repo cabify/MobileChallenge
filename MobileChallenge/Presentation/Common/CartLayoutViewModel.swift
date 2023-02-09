993570c217df39ccfe0bd01a2081c36be64c007e
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-09 20:01:04 -0300

    Prevented remove item from cart to hide items on products list

--- MobileChallenge/Presentation/Common/CartLayoutViewModel.swift
+++ MobileChallenge/Presentation/Common/CartLayoutViewModel.swift
@@ -157,20 +157,13 @@ struct CartLayoutViewModel {
     }
     
     // MARK: - Setter
-    mutating func updateItem(_ item: CartItem, newCartQuantity: Int, hideZero: Bool = false) {
+    mutating func updateItem(_ item: CartItem, newCartQuantity: Int) {
         guard let indexOf = self.items.firstIndex(where: { $0.productType == item.productType }) else { return }
         
         var updatedItems = self.items
-        // Remove item when quantity reaches 0
-        if newCartQuantity == 0, hideZero {
-            updatedItems.remove(at: indexOf)
-            
-        } else {
-            // Update item quantity
-            var updatedItem = item
-            updatedItem.updateCartQuantity(newCartQuantity)
-            updatedItems[indexOf] = updatedItem
-        }
+        var updatedItem = item
+        updatedItem.updateCartQuantity(newCartQuantity)
+        updatedItems[indexOf] = updatedItem
         
         self.items = updatedItems
     }
