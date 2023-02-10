bd68c85019ae60600cbf2ac05a6b84f6e2deb7e9
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-10 13:46:00 -0300

    Mocked a cart repository

---
+++ MobileChallengeTests/Domain/UseCases/Mocks/MockedCartRepository.swift
@@ -0,0 +1,75 @@
+//
+//  MockedCartRepository.swift
+//  MobileChallengeTests
+//
+//  Created by thomas pereira on 10/02/2023.
+//
+
+import Foundation
+import Combine
+
+final class MockedCartRepository {
+    
+    private let error: LocalizedError?
+    
+    init(error: LocalizedError? = nil) {
+        self.error = error
+    }
+}
+
+extension MockedCartRepository: CartRepository {
+    
+    func getCart() -> AnyPublisher<Cart, Error> {
+        Future { promise in
+            if let anError = self.error {
+                promise(.failure(anError))
+                
+            } else {
+                promise(.success(Cart.preview))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func addItem(_ item: Cart.Item) -> AnyPublisher<Int, Error> {
+        Future { promise in
+            if let anError = self.error {
+                promise(.failure(anError))
+                
+            } else {
+                promise(.success(item.quantity + 1))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func removeItem(_ item: Cart.Item) -> AnyPublisher<Int, Error> {
+        Future { promise in
+            if let anError = self.error {
+                promise(.failure(anError))
+                
+            } else if item.quantity > 0 {
+                promise(.success(item.quantity - 1))
+                
+            } else {
+                promise(.failure(TestsError.insufficientQuantity))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    func clearCart() -> AnyPublisher<Cart, Error> {
+        Future { promise in
+            if let anError = self.error {
+                promise(.failure(anError))
+                
+            } else {
+                let cartItems = Cart.preview.items.compactMap {
+                    Cart.Item(code: $0.code, name: $0.name, quantity: 0, price: $0.price)
+                }
+                promise(.success(Cart(items: cartItems)))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+}
