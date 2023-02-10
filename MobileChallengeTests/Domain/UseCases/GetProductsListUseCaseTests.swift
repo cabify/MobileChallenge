ea10d1ef20a9e8f4acf925a0496ddcd670dbc767
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-10 13:02:26 -0300

    Tested get products list use case

---
+++ MobileChallengeTests/Domain/UseCases/GetProductsListUseCaseTests.swift
@@ -0,0 +1,41 @@
+//
+//  GetProductsListUseCaseTests.swift
+//  MobileChallengeTests
+//
+//  Created by thomas pereira on 10/02/2023.
+//
+
+import XCTest
+import Combine
+@testable import MobileChallenge
+
+final class GetProductsListUseCaseTests: XCTestCase {
+    
+    private var cancellables: Set<AnyCancellable>!
+    
+    override func setUp() {
+        super.setUp()
+        self.cancellables = []
+    }
+    
+    // MARK: - Success
+    func testGetProductsListUseCase_whenSuccessfullyFetchesProductsList_thenValidateProducts() throws {
+        // Given
+        let mockedRepository = MockedProductsListRepository()
+        let getProductsListUseCase = DefaultGetProductsListUseCase(productsListRepository: mockedRepository)
+        
+        // When
+        let productList = try awaitPublisher(getProductsListUseCase.getProductsList())
+        
+        // Then
+        XCTAssertEqual(productList.products.count, 3)
+        // First product
+        XCTAssertEqual(productList.products.first?.code, "VOUCHER")
+        XCTAssertEqual(productList.products.first?.name, "Cabify Voucher")
+        XCTAssertEqual(productList.products.first?.price, 5.0)
+        // Last product
+        XCTAssertEqual(productList.products.last?.code, "MUG")
+        XCTAssertEqual(productList.products.last?.name, "Cabify Coffee Mug")
+        XCTAssertEqual(productList.products.last?.price, 7.5)
+    }
+}
