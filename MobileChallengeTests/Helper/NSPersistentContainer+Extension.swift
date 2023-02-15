c5266ffee34f05efee6d7768c226082c96355658
thomasccpereira <thomascalmon.cp@gmail.com>
2023-02-15 10:48:13 +0000

    Implemented an extension to destroy persistent store coordinator

---
+++ MobileChallengeTests/Helper/NSPersistentContainer+Extension.swift
@@ -0,0 +1,27 @@
+//
+//  NSPersistentContainer+Extension.swift
+//  MobileChallengeTests
+//
+//  Created by thomas pereira on 15/02/2023.
+//
+
+import Foundation
+import CoreData
+
+extension NSPersistentContainer {
+    
+    func destroyPersistentStore() {
+        
+        guard let storeURL = persistentStoreDescriptions.first?.url,
+              let storeType = persistentStoreDescriptions.first?.type else {
+            return
+        }
+        
+        do {
+            let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: NSManagedObjectModel())
+            try persistentStoreCoordinator.destroyPersistentStore(at: storeURL, ofType: storeType, options: nil)
+        } catch let error {
+            print("failed to destroy persistent store at \(storeURL), error: \(error)")
+        }
+    }
+}
